dbfee10e664abf17f037699977c0400bab157ac5
gitcoder <yo@mail.com>
2026-02-15 18:48:01 +0530

    Added tweet playlist comment like model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,33 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const playlistSchema = new Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    description:{
+        type: String,
+        required:true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref:"User"
+    },
+    video:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+        ],
+
+},{timestamps:true})
+
+
+
+
+
+
+
+
+export const Playlist = mongoose.model("Playlist",playlistSchema)
